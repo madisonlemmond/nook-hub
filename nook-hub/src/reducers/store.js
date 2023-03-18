7fc0257c3325081ea2d968a14e92c26c373b18ea
loliver0228 <loliver0228@gmail.com>
2023-03-18 11:46:22 -0700

    - installed react-redux - Hooked up Reducer to browser-store - Added provider for browser storer context across application

--- nook-hub/src/reducers/store.js
+++ nook-hub/src/reducers/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 //Add additional Reducers here
-import villagersReducer from './villager'
+import VillagersReducer from './villager'
 
 // The store combines/configures reducers into a single reactDOM. The properties or keys provided here dictate the root state names IE villager, fish, etc
 const store = configureStore({
     reducer: {
-        villager: villagersReducer
+        villager: VillagersReducer
     }
 });
 
